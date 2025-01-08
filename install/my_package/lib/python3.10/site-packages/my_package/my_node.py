72546bbbc6e210bb8b62c9be0f024bbd733d7c91
JeanD56 <jean.dubuc56@gmail.com>
2025-01-08 06:57:26 +0100

    premier commit test

---
+++ install/my_package/lib/python3.10/site-packages/my_package/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Bonjour depuis my_package.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ install/py_pubsub/lib/python3.10/site-packages/py_pubsub/pubsub.launch.py
@@ -0,0 +1,12 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+def generate_launch_description():
+        return LaunchDescription([
+        Node(package = "py_pubsub",
+            executable = "talker"
+        ),
+        Node(package = "py_pubsub",
+            executable = "listener"
+        ),
+    ])
